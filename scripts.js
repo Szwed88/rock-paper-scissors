97e7a2319e1c4ee774db42dbd94982292bc8b803
Szwed88 <80152510+Szwed88@users.noreply.github.com>
2026-03-08 20:22:21 +0000

    added functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,39 @@
 console.log ("Hello World");
+
+const humanScore = 0;
+const computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
+function getComputerChoice(){
+
+const number = Math.floor(Math.random() * 3);
+if (number === 0) return "rock";
+else if (number === 1) return "paper";
+else return "scissors";
+
+}
+
+
+function getHumanChoice(){
+
+const choice = prompt( "Please enter your choice: Rock, paper or scissors");
+return choice;
+
+}
+function game(){
+
+ let humanScore = 0;
+ let computerScore = 0;
+
+}
+
+function playRound(humanChoice, computerChoice){
+    
+ const humanChoice = humanChoice.toLowerCase();
+ 
+}
+
